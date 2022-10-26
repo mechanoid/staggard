cb08a48a64b5c7ebc3a756d52d28322a32ffe9cd
Falk Hoppe <falkhoppe81@gmail.com>
2022-10-26 13:51:23 +0200

    cleanup exported types

--- main.ts
+++ main.ts
@@ -1,16 +1,25 @@
-type TemplateStringKeyList = Array<unknown>;
+type TemplateStringKeyList = unknown[];
 
 import { escapeHtml } from "./deps.ts";
 import { TemplateString } from "./template_string.ts";
 
 export type HTMLTemplateGenerator = AsyncGenerator;
+export type HTMLTemplate =
+  | HTMLTemplateGenerator
+  | Promise<HTMLTemplateGenerator>;
 
 export interface ResponseStream {
   write(chunk: Uint8Array): unknown;
   close?(): void;
 }
 
-async function* resolver(parts: TemplateStringKeyList = []): AsyncGenerator {
+const isAsyncIterator = (thing: unknown) =>
+  typeof (thing as AsyncGenerator<unknown>)[Symbol.asyncIterator] ===
+    "function";
+
+async function* resolver(
+  parts: unknown[] = [],
+): HTMLTemplateGenerator {
   for (const part of parts) {
     if ((part as TemplateString).isTemplateString) { // just return the static string parts of template literals
       yield part;
@@ -26,14 +35,12 @@ async function* resolver(parts: TemplateStringKeyList = []): AsyncGenerator {
       // Itself might be a new `html` generator, so that we pass yield then to it.
       const resolved = await part;
 
-      if (
-        // sometimes an asynchronous call results also in an `html` tagged template.
-        // Such should not be escaped but handed to yield. (E.g. when a canceled promise results in alternative html`...` content.)
-        typeof (resolved as AsyncGenerator<unknown>)[Symbol.asyncIterator] ===
-          "function"
-      ) {
+      // sometimes an asynchronous call results also in an `html` tagged template.
+      // Such should not be escaped but handed to yield. (E.g. when a canceled promise results in alternative html`...` content.)
+      if (isAsyncIterator(resolved)) {
         yield* (resolved as HTMLTemplateGenerator);
       } else {
+        // anything else should be treated as a string and therefore be escaped for control signs
         yield escapeHtml(resolved as string);
       }
     }
@@ -57,16 +64,16 @@ const mixUp = (a1: TemplateStringsArray, a2: TemplateStringKeyList = []) => {
 export const html = (
   strings: TemplateStringsArray,
   ...keys: TemplateStringKeyList
-) => resolver(mixUp(strings, keys));
+): HTMLTemplateGenerator => resolver(mixUp(strings, keys));
 
 // renderer to a fixed output string, resolving all async values provided to the template keys
 export const renderToString = async (
-  template: HTMLTemplateGenerator,
+  template: HTMLTemplate,
 ): Promise<string> => {
   const result = [];
 
   while (true) {
-    const part = await template.next();
+    const part = await (await template).next();
     result.push(part.value);
     // console.log(part.value);
 
@@ -80,12 +87,12 @@ export const renderToString = async (
 
 export const renderToStream = async (
   stream: ResponseStream,
-  template: HTMLTemplateGenerator,
+  template: HTMLTemplate,
 ) => {
   const encoder = new TextEncoder();
   while (true) {
     try {
-      const part = await template.next();
+      const part = await (await template).next();
       stream.write(encoder.encode(part.value));
 
       if (part.done) {
