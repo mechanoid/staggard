f1cf4c30b4b6c99046081f8a7a1d6bfc48ff992f
Falk Hoppe <falk@hey.com>
2023-05-30 22:12:43 +0200

    update lock

--- synchronous_rendering_test.ts
+++ synchronous_rendering_test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from "https://deno.land/std@0.154.0/testing/asserts.ts";
+import { assertEquals } from "std/testing/asserts.ts";
 import { attr, html, mixUp, renderToString } from "./main.ts";
 import { TemplateString } from "./template_string.ts";
 
